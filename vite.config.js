a4c5c32e9183cef0b44d8e7fd7aefc185c20ba57
jiyoung145900-png <jiyoung145900@gmail.com>
2026-07-03 00:51:25 +0900

    vite base 경로 원복

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/daisy-vip/',   // package.json의 homepage 경로와 일치시킴
+  base: '/',   // package.json의 homepage 경로와 일치시킴
   server: {
     port: 5174,
     strictPort: true
